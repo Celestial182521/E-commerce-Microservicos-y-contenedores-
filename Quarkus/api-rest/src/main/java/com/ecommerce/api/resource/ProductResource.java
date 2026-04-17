b8c9af09de7dfd53e226673af19cea0fcdf1b9d4
Codetfj <143056473+Codetfj@users.noreply.github.com>
2026-04-16 23:43:12 -0600

    PASO  5
    paso 5 de creacion de quarkus

---
+++ Quarkus/api-rest/src/main/java/com/ecommerce/api/resource/ProductResource.java
@@ -0,0 +1,14 @@
+package com.ecommerce.api.resource;
+import  jakarta.ws.rs.Consumes;
+import  jakarta.ws.rs.Path;
+import  jakarta.ws.rs.Produces;
+import  jakarta.ws.rs.core.MediaType;
+
+@Path("/api/v1") // Prefijo base de la API
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProductResource 
+{
+    
+
+}
