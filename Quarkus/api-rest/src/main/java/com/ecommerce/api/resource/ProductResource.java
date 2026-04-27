99f33318e7f574e49ec9d093bdd64ca81240859c
Codetfj <143056473+Codetfj@users.noreply.github.com>
2026-04-26 21:25:58 -0600

    Update ProductResource.java

--- Quarkus/api-rest/src/main/java/com/ecommerce/api/resource/ProductResource.java
+++ Quarkus/api-rest/src/main/java/com/ecommerce/api/resource/ProductResource.java
@@ -1,8 +1,27 @@
 package com.ecommerce.api.resource;
-import  jakarta.ws.rs.Consumes;
-import  jakarta.ws.rs.Path;
-import  jakarta.ws.rs.Produces;
-import  jakarta.ws.rs.core.MediaType;
+import  com.ecommerce.api.model.Cart;
+import  com.ecommerce.api.model.CartItemRequest;
+import  com.ecommerce.api.model.CartItemsItemIdPatchRequest;
+import  com.ecommerce.api.model.CartProduct;
+import  com.ecommerce.api.model.Order;
+import  com.ecommerce.api.model.OrdersIdPatchRequest;
+import  com.ecommerce.api.model.OrderItem;
+import  com.ecommerce.api.model.PaymentRequest;
+import  com.ecommerce.api.model.Products;
+import  com.ecommerce.api.model.ProductUpdate;
+import  com.ecommerce.api.model.UserCreate;
+import  com.ecommerce.api.model.Users;
+import  com.ecommerce.api.model.UserUpdate;
+
+import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/api/v1") // Prefijo base de la API
 @Produces(MediaType.APPLICATION_JSON)
